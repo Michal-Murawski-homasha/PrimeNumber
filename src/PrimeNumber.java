a1a6cafb124c26c4500356d436d6f5111419b3a1
Michał Murawski <homasha@gmail.com>
2022-08-12 19:59:53 +0200

    Adds numbers from table to formula

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20};
         System.out.println("Some numbers " + Arrays.toString(numbers));
 
-        int x = 19;
-        if ((x / x == 1) && (x / 1 == x) && (x % 2 != 0) && (x % 3 != 0)) {
-            System.out.println(x + " is prime number");
-        } else {
-            System.out.println(x + " is not prime number");
+//        int x = 19;
+        for (int x = 0; x < numbers.length; x++) {
+            if ((numbers[x] / numbers[x] == 1) && (numbers[x] / 1 == numbers[x]) && (numbers[x] % 2 != 0) && (numbers[x] % 3 != 0)) {
+                System.out.println(numbers[x] + " is prime number");
+            } else {
+                System.out.println(numbers[x] + " is not prime number");
+            }
         }
     }
 }
