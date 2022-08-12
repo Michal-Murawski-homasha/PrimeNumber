b33cb5761e3aa313775cd92cf5e111a7f30177fa
Michał Murawski <homasha@gmail.com>
2022-08-12 20:42:46 +0200

    Changed formula for prime number

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -5,12 +5,20 @@ public class PrimeNumber {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20};
         System.out.println("Some numbers " + Arrays.toString(numbers));
 
-//        int x = 19;
         for (int x = 0; x < numbers.length; x++) {
-            if ((numbers[x] / numbers[x] == 1) && (numbers[x] / 1 == numbers[x]) && (numbers[x] % 2 != 0) && (numbers[x] % 3 != 0)) {
-                System.out.println(numbers[x] + " is prime number");
+            boolean first = true;
+            if (numbers[x] < 2) {
+                first = false;
             } else {
-                System.out.println(numbers[x] + " is not prime number");
+                for (int y = 2; y <= numbers[x] / 2; y++) {
+                    if (numbers[x] % y == 0) {
+                        first = false;
+                        System.out.println(numbers[x] + " is not prime number");
+                    }
+                }
+            }
+            if (first) {
+                System.out.println(numbers[x] + " is prime number");
             }
         }
     }
