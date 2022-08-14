fc4ff640361a140ce20fd1309250b55dce2b47fd
Michał Murawski <homasha@gmail.com>
2022-08-14 09:05:07 +0200

    Numbers from number table divides by numbers from dividerNumber table

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20};
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
         int[] dividerNumbers = {2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println("Some numbers " + Arrays.toString(numbers));
 
@@ -12,7 +12,7 @@ public class PrimeNumber {
                 System.out.println(numbers[x] + " is not prime number");
                 first = false;
             } else {
-                for (int y = 2; y <= dividerNumbers[y] / 2; y++) {
+                for (int y = 2; y <= numbers[x] / dividerNumbers[y]; y++) {
                     if (numbers[x] % y == 0) {
                         first = false;
                         System.out.println(numbers[x] + " is not prime number for y = " + y);
