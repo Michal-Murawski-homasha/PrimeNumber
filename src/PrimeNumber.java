ca5043f6d5205db6729a5f5ed73525c20cb2fcf9
Michał Murawski <homasha@gmail.com>
2022-08-14 08:52:06 +0200

    Adds divider numbers table

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class PrimeNumber {
     public static void main(String[] args) {
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20};
+        int[] dividerNumbers = {2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println("Some numbers " + Arrays.toString(numbers));
 
         for (int x = 0; x < numbers.length; x++) {
@@ -11,10 +12,10 @@ public class PrimeNumber {
                 System.out.println(numbers[x] + " is not prime number");
                 first = false;
             } else {
-                for (int y = 2; y <= numbers[x] / 2; y++) {
+                for (int y = 2; y <= dividerNumbers[y] / 2; y++) {
                     if (numbers[x] % y == 0) {
                         first = false;
-                        System.out.println(numbers[x] + " is not prime number");
+                        System.out.println(numbers[x] + " is not prime number for y = " + y);
                     }
                 }
             }
