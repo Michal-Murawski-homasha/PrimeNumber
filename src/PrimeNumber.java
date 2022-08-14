fb43c0f2b3ff7b67da2a54480707778767fb830e
Michał Murawski <homasha@gmail.com>
2022-08-14 08:07:01 +0200

    Prints small than two

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20};
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20};
         System.out.println("Some numbers " + Arrays.toString(numbers));
 
         for (int x = 0; x < numbers.length; x++) {
             boolean first = true;
             if (numbers[x] < 2) {
+                System.out.println(numbers[x] + " is not prime number");
                 first = false;
             } else {
                 for (int y = 2; y <= numbers[x] / 2; y++) {
