486a365932c2af7bd52922817205ad70b934e542
Michał Murawski <homasha@gmail.com>
2022-08-15 09:10:09 +0200

    Remove second table

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class PrimeNumber {
     public static void main(String[] args) {
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17,18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
-        int[] dividerNumbers = {2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println("Some numbers " + Arrays.toString(numbers));
 
         for (int x = 0; x < numbers.length; x++) {
@@ -12,7 +11,7 @@ public class PrimeNumber {
                 System.out.println(numbers[x] + " is not prime number");
                 first = false;
             } else {
-                for (int y = 2; y <= numbers[x] / dividerNumbers[y]; y++) {
+                for (int y = 2; y <= numbers[x] / y; y++) {
                     if (numbers[x] % y == 0) {
                         first = false;
                         System.out.println(numbers[x] + " is not prime number for y = " + y);
